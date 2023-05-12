6036e4488ccb1bda99486bd69ec6d71a4a2eef51
uken49 <heldergavioes@hotmail.com>
2023-05-12 12:15:39 -0300

    data.sql: Changing attributes of the Address entity

--- data/data.sql
+++ data/data.sql
@@ -11,13 +11,13 @@ CREATE TABLE IF NOT EXISTS CLIENT(
 
 CREATE TABLE IF NOT EXISTS ADDRESS(
         id uuid NOT NULL,
+        zipCode varchar(9),
         street varchar(256),
+        complement varchar(256),
         neighborhood varchar(100),
         city varchar(100),
         state varchar(100),
         house_number integer,
-        complement varchar(256),
-        cep varchar(9),
         created_at timestamp,
         updated_at timestamp,
         PRIMARY KEY (id)
