0544fe4a1c9434dcf19b8dd8660ab13ffef2f035
uken49 <heldergavioes@hotmail.com>
2023-05-15 10:00:20 -0300

    add feignclient dependency and enable in main

--- src/main/java/com/example/apicliente/ApiClienteApplication.java
+++ src/main/java/com/example/apicliente/ApiClienteApplication.java
@@ -2,7 +2,9 @@ package com.example.apicliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiClienteApplication {
 
