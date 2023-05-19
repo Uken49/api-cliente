c1a61f1952dc5c77bdc82536458a52459e2a9af1
uken49 <heldergavioes@hotmail.com>
2023-05-19 10:01:04 -0300

    Trocando extends da exception

--- src/main/java/com/example/apicliente/handler/Execpetion/ClientNotFoundException.java
+++ src/main/java/com/example/apicliente/handler/Execpetion/ClientNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.apicliente.handler.Execpetion;
 
-public class ClientNotFoundException extends Exception {
+public class ClientNotFoundException extends RuntimeException {
 
     public ClientNotFoundException(String message) {
         super(message);
